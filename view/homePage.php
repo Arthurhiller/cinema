42282bb2aadcd8fa9f246f651a448335be69f03c
Arkang67 <arthur.entrepreneuriat@hotmail.com>
2022-10-25 14:30:33 +0200

    navbar/li/a (bootstrap)

--- view/homePage.php
+++ view/homePage.php
@@ -1,10 +1,24 @@
 <?php ?>
-<nav>
-    <a href="index.php?action=films">Films</a>
-    <a href="index.php?action=realisateurs">Realisateurs</a>
-    <a href="index.php?action=categories">Catégories</a>
-    <a href="index.php?action=acteurs">Acteurs</a>
-    <a href="index.php?action=acteursRole&id=">Test</a>
-    <a href="index.php?action=roles">Role</a>
-    <a href="index.php?action=castings">Casting</a>
+<nav class="nav nav-tabs justify-content-center">
+    <li class="nav-item">
+        <a class="nav-link active" href="index.php?action=films">Films</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" href="index.php?action=realisateurs">Realisateurs</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" href="index.php?action=categories">Catégories</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" href="index.php?action=acteurs">Acteurs</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" href="index.php?action=acteursRole&id=">Test</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" href="index.php?action=roles">Role</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link active" href="index.php?action=castings">Casting</a>
+    </li>
 </nav>
