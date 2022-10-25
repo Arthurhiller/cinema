75d5b9e0db6be531ef7d126602f5fe96aadff3a9
Arkang67 <arthur.entrepreneuriat@hotmail.com>
2022-10-25 15:51:05 +0200

    Modification sur la view de tous les acteurs

--- view/list/acteurs.php
+++ view/list/acteurs.php
@@ -2,31 +2,24 @@
 ob_start();
 ?>
 
-<h2>Acteurs</h2>
-<?php
-foreach($stmtListActeurs->fetchall() as $acteur): ?>
-<table>
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Nom</th>
-            <th>Prenom</th>
-            <th>Sexe</th>
-            <th>Date de naissance</th>
-        </tr>
-    </thead>
-    <tbody>
-        <td><a href="index.php?action=unActeur&id=<?= $acteur['id_acteur'] ?>">ICI</a></td>
-        <td><?= $acteur['nom'] ?></td>
-        <td><?= $acteur['prenom'] ?></td>
-        <td><?= $acteur['sexe'] ?></td>
-        <td><?= $acteur['date_naissance'] ?></td>
-    </tbody>
-</table>
-<?php
-endforeach;
-?>
-
+<h2>Tous les acteurs</h2>
+<div class="row">
+    <?php foreach($stmtListActeurs->fetchall() as $acteur): ?>
+    <div class="flex-wrap">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h4 class="card-title"> <?= $acteur['nom'] ?></h4>
+                <h5 class="card-subtitle"><?= $acteur['prenom'] ?></h5>
+                <p class="card-text">
+                    <p>le sexe : <?= $acteur['sexe'] ?></p>
+                    <p>la date de naissance de naissance : <?= $acteur['date_naissance'] ?></p>
+                </p>
+                <a href="index.php?action=unActeur&id=<?= $acteur['id_acteur'] ?>" class="btn btn-primary">Detail</a>
+            </div>
+        </div>
+    </div>
+    <?php endforeach; ?>
+</div>
 
 <?php
 $contenu = ob_get_clean();
